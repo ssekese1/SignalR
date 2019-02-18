7d14e791ebf3a76f32a89c447ca4f27dc9f8b513
Senaki <Senaki@bbd.co.za>
2019-02-18 09:25:13 +0200

    refactoring

--- SignalRTesting/SignalRTesting/Scripts/Learning.js
+++ SignalRTesting/SignalRTesting/Scripts/Learning.js
@@ -1,18 +1,20 @@
-﻿
-//immediately invoked function expression
-(function()
-{
-    $.connection.hub.start()//make connection
-.done(function () {
-    console.log("Yay...it works!");
+﻿    //immediately invoked function expression
+   ( function()
+    {
+        var myHub = $.connection.firstHub;
+        $.connection.hub.start()//make connection
+    .done(function () {//if connection succeeds
+        WriteToPage("Yay...it works!");
+        myHub.server.learning("connected!");//choosing server to connect to
+    })
+    .fail(function () { WriteToPage("Error writing to file!"); })//if connection fails
 
+        myHub.client.learning = function (message) {//assigning clients.learning to the above func
+            WriteToPage("connected!");
+        }
+        var WriteToPage = function(message)
+        {
+            $("#msg").append(message + " <br />");//why cant i just pass a message and not a string????
+        }
 
-    $.connection.firstHub.server.learning("connected!");//choosing server to connect to
-})//if connection succeeds
-.fail(function () { alert("You fucked up!"); })//if connection fails
-
-    $.connection.firstHub.client.learning = function (message) {//assigning clients.learning to the above func
-        $("#msg").append("connected! <br />");//why cant i just passa message and not a string????
-
-    }
-})()
+    })()
